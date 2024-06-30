6bab7a8c7c76b683ca7f72b322bf4f934852c32c
Marcel Lee <chungchung1315@gmail.com>
2024-06-30 21:31:37 +0900

    chore: Add spring boot project

---
+++ src/main/java/com/project/trainingdiary/TrainingDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.project.trainingdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingDiaryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrainingDiaryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/project/trainingdiary/TrainingDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trainingdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingDiaryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
