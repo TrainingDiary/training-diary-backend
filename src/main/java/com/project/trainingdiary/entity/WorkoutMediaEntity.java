3a5c8066e68aa825ae4bd26d80aca520449f87e6
Hyunju Lee <guswnee00@gmail.com>
2024-07-01 23:09:20 +0900

    feat: 운동 미디어 초기 엔티티

---
+++ src/main/java/com/project/trainingdiary/entity/WorkoutMediaEntity.java
@@ -0,0 +1,25 @@
+package com.project.trainingdiary.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "WorkoutMedia")
+public class WorkoutMediaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+}
