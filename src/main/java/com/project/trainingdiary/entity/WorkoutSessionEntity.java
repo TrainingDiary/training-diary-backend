8ff5a9342ae5513d9df5ef83f231bf941e9998a6
Hyunju Lee <guswnee00@gmail.com>
2024-07-01 23:10:23 +0900

    feat: 운동 세션 엔티티

--- src/main/java/com/project/trainingdiary/entity/WorkoutSessionEntity.java
+++ src/main/java/com/project/trainingdiary/entity/WorkoutSessionEntity.java
@@ -4,6 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,5 +27,24 @@ public class WorkoutSessionEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  private LocalDate sessionDate;
+
+  private int sessionNumber;
+
+  @Lob
+  private String specialNote;
+
+  @OneToMany(mappedBy = "workoutSession")
+  private List<WorkoutEntity> workouts;
+
+  @OneToMany(mappedBy = "workoutSession")
+  private List<WorkoutMediaEntity> workoutMedia;
+
+  @OneToOne
+  private ScheduleEntity schedule;
+
+  @ManyToOne
+  private PtContractEntity ptContract;
 
 }
