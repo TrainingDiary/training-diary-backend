a366161b135790c8efe9e8f4c3ad2b6c0f6f3a48
Marcel Lee <chungchung1315@gmail.com>
2024-07-02 22:48:19 +0900

    fix: 개발 시 에러 stack trace 출력

--- src/main/java/com/project/trainingdiary/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/trainingdiary/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.project.trainingdiary.dto.response.CommonResponse;
 import com.project.trainingdiary.exception.GlobalException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  @Value("${print-stack-trace:false}")
+  private boolean printStackTrace;
+
   @ExceptionHandler(GlobalException.class)
   public CommonResponse<?> handler(GlobalException e, HttpServletRequest request) {
 
@@ -19,6 +23,10 @@ public class GlobalExceptionHandler {
         "GlobalException, {}, {}, {}",
         e.getHttpStatus(), e.getMessage(), request.getRequestURI()
     );
+
+    if (printStackTrace) {
+      e.printStackTrace();
+    }
 
     return new CommonResponse<>(e.getHttpStatus(), e.getMessage());
   }
@@ -30,6 +38,10 @@ public class GlobalExceptionHandler {
         "Exception, {}, {}, {}",
         HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), request.getRequestURI()
     );
+
+    if (printStackTrace) {
+      e.printStackTrace();
+    }
 
     return new CommonResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러");
   }
