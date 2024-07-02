b09f5473c2fc7040343db0f24473175d73412875
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-03 00:35:17 +0900

    feat: 회원 관련 컨트롤러 추가

--- src/main/java/com/project/trainingdiary/controller/UserController.java
+++ src/main/java/com/project/trainingdiary/controller/UserController.java
@@ -1,5 +1,48 @@
 package com.project.trainingdiary.controller;
 
+import com.project.trainingdiary.dto.request.EmailDuplicateCheckRequestDto;
+import com.project.trainingdiary.dto.request.SendVerificationEmailRequestDto;
+import com.project.trainingdiary.dto.request.VerifyCodeRequestDto;
+import com.project.trainingdiary.dto.response.CommonResponse;
+import com.project.trainingdiary.model.SuccessMessage;
+import com.project.trainingdiary.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
 public class UserController {
 
+  private final UserService userService;
+
+  @GetMapping("/check-duplicate-email")
+  public CommonResponse<?> checkDuplicateEmail(
+      @RequestBody @Valid EmailDuplicateCheckRequestDto dto
+  ) {
+    userService.checkDuplicateEmail(dto);
+    return CommonResponse.success(SuccessMessage.NO_DUPLICATE_EMAIL);
+  }
+
+  @PostMapping("/send-verification-email")
+  public ResponseEntity<?> sendVerificationEmail(
+      @RequestBody @Valid SendVerificationEmailRequestDto dto
+  ) {
+    userService.sendVerificationEmail(dto);
+    return CommonResponse.success(SuccessMessage.SENT_VERIFICATION_SUCCESS);
+  }
+
+  @PostMapping("/check-verification-code")
+  public CommonResponse<?> verifyCode(
+      @RequestBody @Valid VerifyCodeRequestDto dto
+  ) {
+    userService.checkVerificationCode(dto);
+    return CommonResponse.success(SuccessMessage.VERIFICATION_SUCCESS);
+  }
 }
