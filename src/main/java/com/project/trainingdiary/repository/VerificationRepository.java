5281b367076528a8ae6275ef2aa88699e167b298
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-03 00:33:40 +0900

    feat: repository 구현

---
+++ src/main/java/com/project/trainingdiary/repository/VerificationRepository.java
@@ -0,0 +1,10 @@
+package com.project.trainingdiary.repository;
+
+import com.project.trainingdiary.entity.VerificationEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationRepository extends JpaRepository<VerificationEntity, Long> {
+
+  Optional<VerificationEntity> findByEmail(String email);
+}
