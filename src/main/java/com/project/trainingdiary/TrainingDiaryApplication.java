835e858fbd75cebc0b49d600fa762703ae2ef53d
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-03 00:34:29 +0900

    feat: auditing 받을떄 설정 추가

--- src/main/java/com/project/trainingdiary/TrainingDiaryApplication.java
+++ src/main/java/com/project/trainingdiary/TrainingDiaryApplication.java
@@ -2,8 +2,10 @@ package com.project.trainingdiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TrainingDiaryApplication {
 
   public static void main(String[] args) {
