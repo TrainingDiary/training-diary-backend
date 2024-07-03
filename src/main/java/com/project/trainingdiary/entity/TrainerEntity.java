2edb13b21313bcb78c4f5e1184b3d3b70f46b292
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-03 18:16:36 +0900

    refactor: Enumerated static import

--- src/main/java/com/project/trainingdiary/entity/TrainerEntity.java
+++ src/main/java/com/project/trainingdiary/entity/TrainerEntity.java
@@ -6,7 +6,6 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import com.project.trainingdiary.model.UserRoleType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
