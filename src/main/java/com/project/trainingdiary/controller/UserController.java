e80c33626f930486909a2f5b890d25f5d6faa5aa
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-03 18:17:05 +0900

    refactor: 중복확인 API랑 이메일 인증 번호 보내기 API 연동

--- src/main/java/com/project/trainingdiary/controller/UserController.java
+++ src/main/java/com/project/trainingdiary/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.project.trainingdiary.controller;
 
-import com.project.trainingdiary.dto.request.EmailDuplicateCheckRequestDto;
-import com.project.trainingdiary.dto.request.SendVerificationEmailRequestDto;
+import com.project.trainingdiary.dto.request.SendVerificationAndCheckDuplicateRequestDto;
 import com.project.trainingdiary.dto.request.VerifyCodeRequestDto;
 import com.project.trainingdiary.dto.response.CommonResponse;
 import com.project.trainingdiary.model.SuccessMessage;
 import com.project.trainingdiary.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,19 +19,11 @@ public class UserController {
 
   private final UserService userService;
 
-  @GetMapping("/check-duplicate-email")
-  public CommonResponse<?> checkDuplicateEmail(
-      @RequestBody @Valid EmailDuplicateCheckRequestDto dto
+  @PostMapping("/check-duplicate-and-send-verification")
+  public CommonResponse<?> checkDuplicateAndSendVerification(
+      @RequestBody @Valid SendVerificationAndCheckDuplicateRequestDto dto
   ) {
-    userService.checkDuplicateEmail(dto);
-    return CommonResponse.success(SuccessMessage.NO_DUPLICATE_EMAIL);
-  }
-
-  @PostMapping("/send-verification-email")
-  public ResponseEntity<?> sendVerificationEmail(
-      @RequestBody @Valid SendVerificationEmailRequestDto dto
-  ) {
-    userService.sendVerificationEmail(dto);
+    userService.checkDuplicateEmailAndSendVerification(dto);
     return CommonResponse.success(SuccessMessage.SENT_VERIFICATION_SUCCESS);
   }
 
