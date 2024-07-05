bb1ed229f60cc696e1f75511cb4b89d15a2e3c20
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-05 16:19:47 +0900

    feat: 인증코드 삭제 메서드 구현

--- src/main/java/com/project/trainingdiary/repository/VerificationRepository.java
+++ src/main/java/com/project/trainingdiary/repository/VerificationRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface VerificationRepository extends JpaRepository<VerificationEntity, Long> {
 
   Optional<VerificationEntity> findByEmail(String email);
+
+  void deleteByEmail(String email);
 }
