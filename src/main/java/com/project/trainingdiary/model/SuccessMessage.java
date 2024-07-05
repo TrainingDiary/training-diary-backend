2c780ae329f6684c1d67873dbf9d519035c29ef1
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-05 16:40:46 +0900

    feat:예약 추가 메세지 구현

--- src/main/java/com/project/trainingdiary/model/SuccessMessage.java
+++ src/main/java/com/project/trainingdiary/model/SuccessMessage.java
@@ -12,7 +12,8 @@ public enum SuccessMessage {
   SENT_VERIFICATION_SUCCESS(HttpStatus.OK, "인증 코드 보내기 성공."),
   VERIFICATION_SUCCESS(HttpStatus.OK, "인증 성공."),
   SIGN_UP_SUCCESS(HttpStatus.CREATED, "회원가입 성공"),
-  SIGN_IN_SUCCESS(HttpStatus.OK, "로그인 성공");
+  SIGN_IN_SUCCESS(HttpStatus.OK, "로그인 성공"),
+  SCHEDULE_OPEN_SUCCESS(HttpStatus.CREATED, "예약 성공");
 
   private final HttpStatus status;
   private final String message;
