bc08d2186ea915b78327828881b72ac3820dc535
Marcel Lee <chungchung1315@gmail.com>
2024-07-07 01:43:23 +0900

    feat: PT계약 엔티티 작성

---
+++ src/main/java/com/project/trainingdiary/entity/PtContractEntity.java
@@ -0,0 +1,41 @@
+package com.project.trainingdiary.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString(exclude = {"trainer", "trainee"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "pt_contract")
+public class PtContractEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  private int totalSession;
+
+  private LocalDateTime sessionUpdatedAt;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "trainer_id")
+  private TrainerEntity trainer;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "trainee_id")
+  private TraineeEntity trainee;
+}
