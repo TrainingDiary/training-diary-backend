b683d2bce11fc21b24047fdc8d927fdac4f49162
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-08 16:13:57 +0800

    refactor: 이름 수정 (Entity 뒤에 추가)

--- src/main/java/com/project/trainingdiary/repository/BlacklistRepository.java
+++ src/main/java/com/project/trainingdiary/repository/BlacklistRepository.java
@@ -1,10 +1,10 @@
 package com.project.trainingdiary.repository;
 
 import com.project.trainingdiary.entity.BlacklistedTokenEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface BlacklistRepository extends JpaRepository<BlacklistedTokenEntity, Long> {
-    Optional<BlacklistedTokenEntity> findByToken(String token);
+
+  Optional<BlacklistedTokenEntity> findByToken(String token);
 }
