07d3614da0c3bf5de0296a203225bec9d1ea0848
Hyunju Lee <guswnee00@gmail.com>
2024-07-09 09:11:19 +0900

    feat: 운동 일지 관련 repo

---
+++ src/main/java/com/project/trainingdiary/repository/WorkoutMediaRepository.java
@@ -0,0 +1,8 @@
+package com.project.trainingdiary.repository;
+
+import com.project.trainingdiary.entity.WorkoutMediaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkoutMediaRepository extends JpaRepository<WorkoutMediaEntity, Long> {
+
+}
