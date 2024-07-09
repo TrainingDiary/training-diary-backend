85dbeed2adcc85c95079b2d4b81f04c7f8070d60
Hyunju Lee <guswnee00@gmail.com>
2024-07-09 12:43:36 +0900

    feat: workoutType과 연결 끊고 컬럼 추가

--- src/main/java/com/project/trainingdiary/entity/WorkoutEntity.java
+++ src/main/java/com/project/trainingdiary/entity/WorkoutEntity.java
@@ -6,8 +6,6 @@ import com.project.trainingdiary.dto.request.WorkoutCreateRequestDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,25 +24,27 @@ public class WorkoutEntity extends BaseEntity {
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
+  private String workoutTypeName;
+  private String targetMuscle;
+  private String remarks;
+
   private int weight;
   private int rep;
   private int sets;
   private int time;
   private int speed;
 
-  @OneToOne
-  @JoinColumn(name = "workout_type_id")
-  private WorkoutTypeEntity workoutType;
-
   public static WorkoutEntity toEntity(WorkoutCreateRequestDto dto, WorkoutTypeEntity entity) {
 
     return WorkoutEntity.builder()
+        .workoutTypeName(entity.getName())
+        .targetMuscle(entity.getTargetMuscle())
+        .remarks(entity.getRemarks())
         .weight(dto.getWeight())
         .rep(dto.getRep())
         .sets(dto.getSets())
         .time(dto.getTime())
         .speed(dto.getSpeed())
-        .workoutType(entity)
         .build();
 
   }
