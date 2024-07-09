6a32d29d85e105765a5e9282bcc806074ad7f791
Hyunju Lee <guswnee00@gmail.com>
2024-07-09 12:42:43 +0900

    feat: api 수정

--- src/main/java/com/project/trainingdiary/controller/WorkoutSessionController.java
+++ src/main/java/com/project/trainingdiary/controller/WorkoutSessionController.java
@@ -4,7 +4,6 @@ import com.project.trainingdiary.dto.request.WorkoutSessionCreateRequestDto;
 import com.project.trainingdiary.dto.response.CommonResponse;
 import com.project.trainingdiary.service.WorkoutSessionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +16,11 @@ public class WorkoutSessionController {
 
   private final WorkoutSessionService workoutSessionService;
 
-  @PostMapping("/{id}/workout-session")
+  @PostMapping("workout-sessions")
   public CommonResponse<?> createWorkoutSession(
-      @RequestBody WorkoutSessionCreateRequestDto dto,
-      @PathVariable Long id
+      @RequestBody WorkoutSessionCreateRequestDto dto
   ) {
-    workoutSessionService.createWorkoutSession(dto, id);
+    workoutSessionService.createWorkoutSession(dto);
     return CommonResponse.created();
   }
 
