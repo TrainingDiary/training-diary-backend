2aa475aca02988257931a87c75891a4147c47a33
Hyunju Lee <guswnee00@gmail.com>
2024-07-14 21:42:59 +0900

    feat: 이미지 업로드 후 반환 dto

---
+++ src/main/java/com/project/trainingdiary/dto/response/WorkoutImageResponseDto.java
@@ -0,0 +1,33 @@
+package com.project.trainingdiary.dto.response;
+
+import com.project.trainingdiary.entity.WorkoutMediaEntity;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WorkoutImageResponseDto {
+
+  private Long sessionId;
+  private List<String> originalUrls;
+  private List<String> thumbnailUrls;
+
+  public static WorkoutImageResponseDto fromEntity(List<WorkoutMediaEntity> workoutMediaList,
+      Long sessionId) {
+
+    return WorkoutImageResponseDto.builder()
+        .sessionId(sessionId)
+        .originalUrls(workoutMediaList.stream().map(WorkoutMediaEntity::getOriginalUrl).toList())
+        .thumbnailUrls(workoutMediaList.stream().map(WorkoutMediaEntity::getThumbnailUrl).toList())
+        .build();
+
+  }
+
+}
