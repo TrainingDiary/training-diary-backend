fc56849dda1b03d919b2b52bd010e89aba7404ec
Marcel Lee <chungchung1315@gmail.com>
2024-07-14 15:30:40 +0900

    feat: CustomResponse을 더 이상 사용하지 않도록 함
    - CustomResponse에 deprecated 표시
    - ErrorResponse를 별도로 만들어 에러 응답 시 사용

--- src/main/java/com/project/trainingdiary/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/trainingdiary/handler/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.project.trainingdiary.handler;
 
-import com.project.trainingdiary.dto.response.CommonResponse;
+import com.project.trainingdiary.exception.ErrorResponse;
 import com.project.trainingdiary.exception.GlobalException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.nio.file.AccessDeniedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,24 +15,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(GlobalException.class)
-  public CommonResponse<?> handler(GlobalException e, HttpServletRequest request) {
+  public ResponseEntity<ErrorResponse> handler(GlobalException e, HttpServletRequest request) {
 
     log.error(
         "GlobalException, {}, {}, {}",
         e.getHttpStatus(), e.getMessage(), request.getRequestURI()
     );
 
-    return new CommonResponse<>(e.getHttpStatus(), e.getMessage());
+    ErrorResponse response = new ErrorResponse(e.getHttpStatus().value(), e.getMessage());
+    return new ResponseEntity<>(response, e.getHttpStatus());
   }
 
   @ExceptionHandler(AccessDeniedException.class)
-  public CommonResponse<?> handleAccessDeniedException(AccessDeniedException e,
+  public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e,
       HttpServletRequest request) {
+
     log.error(
         "AccessDeniedException, {}, {}, {}",
         HttpStatus.FORBIDDEN, e.getMessage(), request.getRequestURI()
     );
 
-    return new CommonResponse<>(HttpStatus.FORBIDDEN, "접근이 없습니다.");
+    ErrorResponse response = new ErrorResponse(HttpStatus.FORBIDDEN.value(), "접근 권한이 없습니다.");
+    return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
   }
 }
