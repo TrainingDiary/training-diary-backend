84151cabe88ed90a21eff5375010b6bb66b7b70f
Marcel Lee <chungchung1315@gmail.com>
2024-07-15 14:39:57 +0900

    feat: 에러 응답 형식 수정

--- src/main/java/com/project/trainingdiary/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/trainingdiary/handler/GlobalExceptionHandler.java
@@ -4,15 +4,10 @@ import com.project.trainingdiary.exception.ErrorResponse;
 import com.project.trainingdiary.exception.GlobalException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.nio.file.AccessDeniedException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +16,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  /**
+   * 프로젝트에서 생성한 예외를 처리합니다.
+   *
+   * @param e GlobalException
+   * @return ResponseEntity<ErrorResponse> - 전역 예외 응답
+   */
   @ExceptionHandler(GlobalException.class)
   public ResponseEntity<ErrorResponse> handler(GlobalException e, HttpServletRequest request) {
 
@@ -33,6 +34,12 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(response, e.getHttpStatus());
   }
 
+  /**
+   * 필드 유효성 검사 예외를 처리합니다.
+   *
+   * @param e MethodArgumentNotValidException
+   * @return ResponseEntity<ErrorResponse> - 필드 유효성 검사 오류 응답
+   */
   @ExceptionHandler(AccessDeniedException.class)
   public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e,
       HttpServletRequest request) {
@@ -49,47 +56,38 @@ public class GlobalExceptionHandler {
   /**
    * 유효성 검사 예외를 처리합니다.
    *
-   * @param ex MethodArgumentNotValidException
-   * @return CustomResponse<Map < String, List < String>>> - 유효성 검사 오류 응답
+   * @param e MethodArgumentNotValidException
+   * @return ResponseEntity<ErrorResponse> - 유효성 검사 오류 응답
    */
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, List<String>>> handleValidationExceptions(
-      MethodArgumentNotValidException ex) {
-    List<String> errors = ex.getBindingResult()
-        .getFieldErrors()
-        .stream()
-        .map(FieldError::getDefaultMessage)
-        .toList();
+  public ResponseEntity<ErrorResponse> handleValidationExceptions(
+      MethodArgumentNotValidException e, HttpServletRequest request) {
 
-    log.error("Validation exception: ", ex);
-    return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    log.error(
+        "MethodArgumentNotValidException, {}, {}, {}",
+        HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI()
+    );
+
+    ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 
   /**
    * JSON 형식 오류 예외를 처리합니다.
    *
-   * @param ex HttpMessageNotReadableException
-   * @return CustomResponse<Map < String, List < String>>> - 잘못된 JSON 요청 오류 응답
+   * @param e HttpMessageNotReadableException
+   * @return ResponseEntity<ErrorResponse> - 잘못된 JSON 요청 오류 응답
    */
   @ExceptionHandler(HttpMessageNotReadableException.class)
-  public ResponseEntity<Map<String, List<String>>> handleHttpMessageNotReadableException(
-      HttpMessageNotReadableException ex) {
-    List<String> errors = List.of("Malformed JSON request");
+  public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+      HttpMessageNotReadableException e, HttpServletRequest request) {
 
-    log.error("Malformed JSON request: ", ex);
-    return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-  }
+    log.error(
+        "HttpMessageNotReadableException, {}, {}, {}",
+        HttpStatus.BAD_REQUEST, e.getMessage(), request.getRequestURI()
+    );
 
-
-  /**
-   * 오류 목록을 맵 형식으로 변환합니다.
-   *
-   * @param errors 오류 목록
-   * @return Map<String, List < String>> - 오류 맵
-   */
-  private Map<String, List<String>> getErrorsMap(List<String> errors) {
-    Map<String, List<String>> errorResponse = new HashMap<>();
-    errorResponse.put("errors", errors);
-    return errorResponse;
+    ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 }
