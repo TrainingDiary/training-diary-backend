7670d36dcc71a984fe2fcd12b0e742f3e320f693
H.one <guswnee00@gmail.com>
2024-07-16 22:24:21 +0900

    refactor: toEntity 메소드 위치 변경, @NotBlank 추가

--- src/main/java/com/project/trainingdiary/dto/request/WorkoutTypeCreateRequestDto.java
+++ src/main/java/com/project/trainingdiary/dto/request/WorkoutTypeCreateRequestDto.java
@@ -1,7 +1,5 @@
 package com.project.trainingdiary.dto.request;
 
-import com.project.trainingdiary.entity.TrainerEntity;
-import com.project.trainingdiary.entity.WorkoutTypeEntity;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,14 +12,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class WorkoutTypeCreateRequestDto {
 
-  private Long trainerId;
-
   @NotBlank(message = "운동 종류 이름은 필수 입력사항입니다.")
   private String name;
 
   @NotBlank(message = "운동 종류의 타겟 부위는 필수 입력사항입니다.")
   private String targetMuscle;
 
+  @NotBlank(message = "운동 종류에 대한 설명을 작성해주세요.")
   private String remarks;
 
   private boolean weightInputRequired;
@@ -29,22 +26,6 @@ public class WorkoutTypeCreateRequestDto {
   private boolean setInputRequired;
   private boolean timeInputRequired;
   private boolean speedInputRequired;
-
-  public static WorkoutTypeEntity toEntity(WorkoutTypeCreateRequestDto dto, TrainerEntity entity) {
-
-    return WorkoutTypeEntity.builder()
-        .name(dto.getName())
-        .targetMuscle(dto.getTargetMuscle())
-        .remarks(dto.getRemarks())
-        .weightInputRequired(dto.isWeightInputRequired())
-        .repInputRequired(dto.isRepInputRequired())
-        .setInputRequired(dto.isSetInputRequired())
-        .timeInputRequired(dto.isTimeInputRequired())
-        .speedInputRequired(dto.isSpeedInputRequired())
-        .trainer(entity)
-        .build();
-
-  }
 
 }
 
