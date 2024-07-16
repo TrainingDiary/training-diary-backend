cd1a8c10869dbd509a6b7979be5b1d607d173cc1
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-16 21:02:16 +0800

    feat: test용 @Setter 추가

--- src/main/java/com/project/trainingdiary/entity/PtContractEntity.java
+++ src/main/java/com/project/trainingdiary/entity/PtContractEntity.java
@@ -14,10 +14,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Builder
 @Getter
+@Setter
 @ToString(exclude = {"trainer", "trainee"})
 @NoArgsConstructor
 @AllArgsConstructor
