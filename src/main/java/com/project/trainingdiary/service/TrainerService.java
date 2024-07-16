3b9667e1d563792b8552c4117c94f13152939182
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-16 21:29:24 +0800

    refactor: 메서드마다 주석 추가

--- src/main/java/com/project/trainingdiary/service/TrainerService.java
+++ src/main/java/com/project/trainingdiary/service/TrainerService.java
@@ -31,6 +31,12 @@ public class TrainerService {
   private final InBodyRecordHistoryRepository inBodyRecordHistoryRepository;
   private final PtContractRepository ptContractRepository;
 
+  /**
+   * 트레이니 정보를 조회합니다.
+   *
+   * @param id 트레이니의 ID
+   * @return TraineeInfoResponseDto 트레이니의 정보와 남은 세션 수를 포함한 응답 DTO
+   */
   public TraineeInfoResponseDto getTraineeInfo(Long id) {
     TrainerEntity trainer = getAuthenticatedTrainer();
     TraineeEntity trainee = getTraineeById(id);
@@ -49,6 +55,12 @@ public class TrainerService {
     return TraineeInfoResponseDto.fromEntity(trainee, remainingSessions);
   }
 
+  /**
+   * 새로운 인바디 기록을 추가합니다.
+   *
+   * @param dto 인바디 기록 추가 요청 DTO
+   * @return AddInBodyInfoResponseDto 추가된 인바디 기록을 포함한 응답 DTO
+   */
   public AddInBodyInfoResponseDto addInBodyRecord(AddInBodyInfoRequestDto dto) {
     TrainerEntity trainer = getAuthenticatedTrainer();
     TraineeEntity trainee = getTraineeById(dto.getTraineeId());
@@ -60,6 +72,12 @@ public class TrainerService {
     return AddInBodyInfoResponseDto.fromEntity(inBodyRecord);
   }
 
+  /**
+   * 트레이니 정보를 수정합니다.
+   *
+   * @param dto 트레이니 정보 수정 요청 DTO
+   * @return EditTraineeInfoResponseDto 수정된 트레이니 정보를 포함한 응답 DTO
+   */
   @Transactional
   public EditTraineeInfoResponseDto editTraineeInfo(EditTraineeInfoRequestDto dto) {
     TrainerEntity trainer = getAuthenticatedTrainer();
@@ -72,11 +90,24 @@ public class TrainerService {
     return EditTraineeInfoResponseDto.fromEntity(trainee);
   }
 
+  /**
+   * 트레이니 ID로 트레이니를 조회합니다.
+   *
+   * @param id 트레이니의 ID
+   * @return TraineeEntity 트레이니 엔티티
+   * @throws TraineeNotExistException 트레이니가 존재하지 않을 경우 예외 발생
+   */
   private TraineeEntity getTraineeById(Long id) {
     return traineeRepository.findById(id)
         .orElseThrow(TraineeNotExistException::new);
   }
 
+  /**
+   * 인증된 트레이너를 조회합니다.
+   *
+   * @return TrainerEntity 트레이너 엔티티
+   * @throws TrainerNotFoundException 트레이너가 존재하지 않을 경우 예외 발생
+   */
   private TrainerEntity getAuthenticatedTrainer() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication == null || authentication.getName() == null) {
@@ -87,12 +118,25 @@ public class TrainerService {
         .orElseThrow(TrainerNotFoundException::new);
   }
 
+  /**
+   * 트레이너와 트레이니 간의 계약을 확인합니다.
+   *
+   * @param trainer 트레이너 엔티티
+   * @param trainee 트레이니 엔티티
+   * @throws PtContractNotExistException 계약이 존재하지 않을 경우 예외 발생
+   */
   private void checkContract(TrainerEntity trainer, TraineeEntity trainee) {
     if (!ptContractRepository.existsByTrainerIdAndTraineeId(trainer.getId(), trainee.getId())) {
       throw new PtContractNotExistException();
     }
   }
 
+  /**
+   * 트레이니 정보를 업데이트합니다.
+   *
+   * @param trainee 트레이니 엔티티
+   * @param dto     트레이니 정보 수정 요청 DTO
+   */
   private void updateTraineeInfo(TraineeEntity trainee, EditTraineeInfoRequestDto dto) {
     trainee.setBirthDate(dto.getBirthDate());
     trainee.setGender(dto.getGender());
--- src/main/java/com/project/trainingdiary/service/UserService.java
+++ src/main/java/com/project/trainingdiary/service/UserService.java
@@ -10,6 +10,8 @@ import com.project.trainingdiary.entity.TraineeEntity;
 import com.project.trainingdiary.entity.TrainerEntity;
 import com.project.trainingdiary.entity.VerificationEntity;
 import com.project.trainingdiary.exception.impl.PasswordMismatchedException;
+import com.project.trainingdiary.exception.impl.TraineeEmailDuplicateException;
+import com.project.trainingdiary.exception.impl.TrainerEmailDuplicateException;
 import com.project.trainingdiary.exception.impl.UserNotFoundException;
 import com.project.trainingdiary.exception.impl.VerificationCodeExpiredException;
 import com.project.trainingdiary.exception.impl.VerificationCodeNotMatchedException;
@@ -56,17 +58,33 @@ public class UserService implements UserDetailsService {
   private static final String REFRESH_TOKEN_COOKIE_NAME = "Refresh-Token";
   private static final String ACCESS_TOKEN_COOKIE_NAME = "Access-Token";
 
+  /**
+   * 이메일 중복을 확인하고 인증 코드를 전송합니다.
+   *
+   * @param dto 이메일 중복 확인 및 인증 코드 전송 요청 DTO
+   */
   public void checkDuplicateEmailAndSendVerification(
       SendVerificationAndCheckDuplicateRequestDto dto) {
     validateEmailNotExists(dto.getEmail());
     sendVerificationCode(dto.getEmail());
   }
 
+  /**
+   * 인증 코드를 확인합니다.
+   *
+   * @param dto 인증 코드 확인 요청 DTO
+   */
   public void checkVerificationCode(VerifyCodeRequestDto dto) {
     VerificationEntity verificationEntity = getVerificationEntity(dto.getEmail());
     validateVerificationCode(verificationEntity, dto.getVerificationCode());
   }
 
+  /**
+   * 회원가입을 처리합니다.
+   *
+   * @param dto      회원가입 요청 DTO
+   * @param response HTTP 응답 객체
+   */
   @Transactional
   public void signUp(SignUpRequestDto dto, HttpServletResponse response) {
     VerificationEntity verificationEntity = getVerificationEntity(dto.getEmail());
@@ -79,35 +97,77 @@ public class UserService implements UserDetailsService {
     verificationRepository.deleteByEmail(dto.getEmail());
   }
 
+  /**
+   * 로그인 요청을 처리합니다.
+   *
+   * @param dto      로그인 요청 DTO
+   * @param response HTTP 응답 객체
+   * @return SignInResponseDto 로그인 응답 DTO
+   */
   public SignInResponseDto signIn(SignInRequestDto dto, HttpServletResponse response) {
     UserDetails userDetails = loadUserByUsername(dto.getEmail());
     validatePassword(dto.getPassword(), userDetails.getPassword());
     return generateTokensAndSetCookies(userDetails.getUsername(), response);
   }
 
+  /**
+   * 로그아웃 요청을 처리합니다.
+   *
+   * @param request  HTTP 요청 객체
+   * @param response HTTP 응답 객체
+   */
   public void signOut(HttpServletRequest request, HttpServletResponse response) {
     blacklistAndClearCookies(request, response);
   }
 
+  /**
+   * 이메일 중복을 확인합니다.
+   *
+   * @param email 확인할 이메일
+   * @throws TraineeEmailDuplicateException 트레이니 이메일이 중복되면 예외 발생
+   * @throws TrainerEmailDuplicateException 트레이너 이메일이 중복되면 예외 발생
+   */
   private void validateEmailNotExists(String email) {
-    if (traineeRepository.findByEmail(email).isPresent() || trainerRepository.findByEmail(email)
-        .isPresent()) {
-      throw new UserNotFoundException();
+    if (traineeRepository.findByEmail(email).isPresent()) {
+      throw new TraineeEmailDuplicateException();
+    }
+    if (trainerRepository.findByEmail(email).isPresent()) {
+      throw new TrainerEmailDuplicateException();
     }
   }
 
+  /**
+   * 인증 코드를 전송합니다.
+   *
+   * @param email 인증 코드를 전송할 이메일
+   */
   private void sendVerificationCode(String email) {
     String verificationCode = VerificationCodeGeneratorUtil.generateVerificationCode();
     emailProvider.sendVerificationEmail(email, verificationCode);
     verificationRepository.save(VerificationEntity.of(email, verificationCode));
   }
 
+  /**
+   * 비밀번호와 비밀번호 확인이 일치하는지 확인합니다.
+   *
+   * @param password        비밀번호
+   * @param confirmPassword 비밀번호 확인
+   * @throws PasswordMismatchedException 비밀번호와 비밀번호 확인이 일치하지 않으면 예외 발생
+   */
   private void validatePasswordsMatch(String password, String confirmPassword) {
     if (!password.equals(confirmPassword)) {
       throw new PasswordMismatchedException();
     }
   }
 
+  /**
+   * 인증 코드를 확인합니다.
+   *
+   * @param verificationEntity 인증 엔티티
+   * @param verificationCode   인증 코드
+   * @throws VerificationCodeNotMatchedException 인증 코드가 일치하지 않으면 예외 발생
+   * @throws VerificationCodeExpiredException    인증 코드가 만료되면 예외 발생
+   */
   private void validateVerificationCode(VerificationEntity verificationEntity,
       String verificationCode) {
     if (!verificationEntity.getVerificationCode().equals(verificationCode)) {
@@ -118,10 +178,23 @@ public class UserService implements UserDetailsService {
     }
   }
 
+  /**
+   * 이메일로 인증 엔티티를 조회합니다.
+   *
+   * @param email 이메일
+   * @return VerificationEntity 인증 엔티티
+   * @throws UserNotFoundException 인증 엔티티가 존재하지 않으면 예외 발생
+   */
   private VerificationEntity getVerificationEntity(String email) {
     return verificationRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
   }
 
+  /**
+   * 회원가입 정보를 저장합니다.
+   *
+   * @param dto             회원가입 요청 DTO
+   * @param encodedPassword 인코딩된 비밀번호
+   */
   private void saveUser(SignUpRequestDto dto, String encodedPassword) {
     if (dto.getRole() == UserRoleType.TRAINEE) {
       saveTrainee(dto, encodedPassword);
@@ -132,6 +205,12 @@ public class UserService implements UserDetailsService {
     }
   }
 
+  /**
+   * 트레이니 정보를 저장합니다.
+   *
+   * @param dto             회원가입 요청 DTO
+   * @param encodedPassword 인코딩된 비밀번호
+   */
   private void saveTrainee(SignUpRequestDto dto, String encodedPassword) {
     TraineeEntity trainee = TraineeEntity.builder()
         .name(dto.getName())
@@ -142,6 +221,12 @@ public class UserService implements UserDetailsService {
     traineeRepository.save(trainee);
   }
 
+  /**
+   * 트레이너 정보를 저장합니다.
+   *
+   * @param dto             회원가입 요청 DTO
+   * @param encodedPassword 인코딩된 비밀번호
+   */
   private void saveTrainer(SignUpRequestDto dto, String encodedPassword) {
     TrainerEntity trainer = TrainerEntity.builder()
         .name(dto.getName())
@@ -152,12 +237,26 @@ public class UserService implements UserDetailsService {
     trainerRepository.save(trainer);
   }
 
+  /**
+   * 비밀번호를 확인합니다.
+   *
+   * @param rawPassword     입력한 비밀번호
+   * @param encodedPassword 저장된 인코딩된 비밀번호
+   * @throws WrongPasswordException 비밀번호가 일치하지 않으면 예외 발생
+   */
   private void validatePassword(String rawPassword, String encodedPassword) {
     if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
       throw new WrongPasswordException();
     }
   }
 
+  /**
+   * 토큰을 생성하고 쿠키에 설정합니다.
+   *
+   * @param username 사용자 이름
+   * @param response HTTP 응답 객체
+   * @return SignInResponseDto 로그인 응답 DTO
+   */
   private SignInResponseDto generateTokensAndSetCookies(String username,
       HttpServletResponse response) {
     String accessToken = tokenProvider.createAccessToken(username);
@@ -177,6 +276,12 @@ public class UserService implements UserDetailsService {
     return new SignInResponseDto(accessToken, refreshToken);
   }
 
+  /**
+   * 블랙리스트에 토큰을 추가하고 쿠키를 삭제합니다.
+   *
+   * @param request  HTTP 요청 객체
+   * @param response HTTP 응답 객체
+   */
   private void blacklistAndClearCookies(HttpServletRequest request, HttpServletResponse response) {
     Cookie accessTokenCookie = cookieProvider.getCookie(request, ACCESS_TOKEN_COOKIE_NAME);
     Cookie refreshTokenCookie = cookieProvider.getCookie(request, REFRESH_TOKEN_COOKIE_NAME);
@@ -188,6 +293,11 @@ public class UserService implements UserDetailsService {
     cookieProvider.clearCookie(response, REFRESH_TOKEN_COOKIE_NAME);
   }
 
+  /**
+   * 토큰을 블랙리스트에 추가합니다.
+   *
+   * @param tokenCookie 토큰 쿠키
+   */
   private void blacklistToken(Cookie tokenCookie) {
     if (tokenCookie != null && tokenProvider.validateToken(tokenCookie.getValue())) {
       log.info("블랙리스트에 추가된 토큰: {}", tokenCookie.getValue());
@@ -196,6 +306,13 @@ public class UserService implements UserDetailsService {
     }
   }
 
+  /**
+   * 사용자 이름으로 사용자 정보를 로드합니다.
+   *
+   * @param username 사용자 이름
+   * @return UserDetails 사용자 정보
+   * @throws UsernameNotFoundException 사용자 이름이 존재하지 않으면 예외 발생
+   */
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     return traineeRepository.findByEmail(username)
@@ -204,6 +321,13 @@ public class UserService implements UserDetailsService {
             .orElseThrow(UserNotFoundException::new)));
   }
 
+  /**
+   * 회원 정보를 조회합니다.
+   *
+   * @param id 회원 ID
+   * @return MemberInfoResponseDto 회원 정보 응답 DTO
+   * @throws UserNotFoundException 회원이 존재하지 않으면 예외 발생
+   */
   public MemberInfoResponseDto memberInfo(Long id) {
     return traineeRepository.findById(id)
         .map(trainee -> MemberInfoResponseDto.builder()
