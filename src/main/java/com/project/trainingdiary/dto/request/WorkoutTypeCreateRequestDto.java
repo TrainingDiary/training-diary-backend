3aec70027c93c0501dcc4fe433bae564af5511b1
H.one <guswnee00@gmail.com>
2024-07-17 13:36:56 +0900

    feat: @builder 추가

--- src/main/java/com/project/trainingdiary/dto/request/WorkoutTypeCreateRequestDto.java
+++ src/main/java/com/project/trainingdiary/dto/request/WorkoutTypeCreateRequestDto.java
@@ -2,12 +2,14 @@ package com.project.trainingdiary.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class WorkoutTypeCreateRequestDto {
--- src/main/java/com/project/trainingdiary/dto/request/WorkoutTypeUpdateRequestDto.java
+++ src/main/java/com/project/trainingdiary/dto/request/WorkoutTypeUpdateRequestDto.java
@@ -3,12 +3,14 @@ package com.project.trainingdiary.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class WorkoutTypeUpdateRequestDto {
