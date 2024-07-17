e3de95fe770eae8e265a02509e785d699bc77e63
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-17 21:49:18 +0800

    feat: Validation 부분 추가 및 response값 수정

--- src/main/java/com/project/trainingdiary/dto/response/SignInResponseDto.java
+++ src/main/java/com/project/trainingdiary/dto/response/SignInResponseDto.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignInResponseDto {
 
-  private String accessToken;
-  private String refreshToken;
+  private String email;
+  private String role;
 }
