0e32ab24c8a8d69bbfba107db0bde92e70f7ab20
Marcel Lee <chungchung1315@gmail.com>
2024-07-18 16:18:39 +0900

    feat: 날짜, 시간 입력 부분에 NotNull 어노테이션 추가

--- src/main/java/com/project/trainingdiary/model/ScheduleDateTimes.java
+++ src/main/java/com/project/trainingdiary/model/ScheduleDateTimes.java
@@ -2,6 +2,7 @@ package com.project.trainingdiary.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -18,9 +19,11 @@ import lombok.Setter;
 @Builder
 public class ScheduleDateTimes {
 
-  @Schema(example = "2024-07-17", requiredMode = RequiredMode.REQUIRED)
+  @NotNull
+  @Schema(example = "2024-07-17")
   private LocalDate startDate;
 
-  @Schema(type = "array", example = "[\"10:00\", \"14:00\"]", requiredMode = RequiredMode.REQUIRED)
+  @NotNull
+  @Schema(type = "array", example = "[\"10:00\", \"14:00\"]")
   private List<LocalTime> startTimes;
 }
