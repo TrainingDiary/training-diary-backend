920b6595548a607c70e4399e9a88e58ab35f8dd6
Hyunju Lee <guswnee00@gmail.com>
2024-07-18 10:23:39 +0900

    feat: 운동 일지 수정을 위한 dto 생성

---
+++ src/main/java/com/project/trainingdiary/dto/request/WorkoutSessionUpdateRequestDto.java
@@ -0,0 +1,26 @@
+package com.project.trainingdiary.dto.request;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkoutSessionUpdateRequestDto {
+
+  private Long sessionId;
+
+  private LocalDate sessionDate;
+  private int sessionNumber;
+  private String specialNote;
+
+  private List<WorkoutUpdateRequestDto> workouts;
+
+}
