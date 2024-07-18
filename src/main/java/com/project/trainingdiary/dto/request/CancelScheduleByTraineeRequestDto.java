f720982d4a8edeaf3e7520245240d9a9ec1a50e5
Marcel Lee <chungchung1315@gmail.com>
2024-07-18 11:58:03 +0900

    fix: 기본 생성자가 없으니 인스턴스 생성 에러가 나서 추가함
    - 요청이 들어올 때 기본 생성자가 없으면 인스턴스 생성에 실패함

--- src/main/java/com/project/trainingdiary/dto/request/CancelScheduleByTraineeRequestDto.java
+++ src/main/java/com/project/trainingdiary/dto/request/CancelScheduleByTraineeRequestDto.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CancelScheduleByTraineeRequestDto {
 
   @NotNull(message = "일정 id를 입력해주세요")
