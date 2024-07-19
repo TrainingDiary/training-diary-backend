890df2e0689e5a27dc6b11acc277175ef05e1dd4
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-19 23:42:33 +0800

    feat: Paging 기능 추가

--- src/main/java/com/project/trainingdiary/repository/DietRepository.java
+++ src/main/java/com/project/trainingdiary/repository/DietRepository.java
@@ -1,8 +1,11 @@
 package com.project.trainingdiary.repository;
 
 import com.project.trainingdiary.entity.DietEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DietRepository extends JpaRepository<DietEntity, Long> {
 
+  Page<DietEntity> findByTraineeId(Long traineeId, Pageable pageable);
 }
