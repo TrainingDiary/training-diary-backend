33ca8cf33f267aa79c6729996e57312a57dc2bca
Hyunju Lee <guswnee00@gmail.com>
2024-07-20 14:22:06 +0900

    docs: swagger 문서 내용 추가

--- src/main/java/com/project/trainingdiary/controller/WorkoutSessionController.java
+++ src/main/java/com/project/trainingdiary/controller/WorkoutSessionController.java
@@ -9,6 +9,11 @@ import com.project.trainingdiary.dto.response.WorkoutSessionListResponseDto;
 import com.project.trainingdiary.dto.response.WorkoutSessionResponseDto;
 import com.project.trainingdiary.dto.response.WorkoutVideoResponseDto;
 import com.project.trainingdiary.service.WorkoutSessionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +21,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +34,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "4 - Workout Session API", description = "운동 일지를 위한 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/workout-sessions")
@@ -35,6 +42,17 @@ public class WorkoutSessionController {
 
   private final WorkoutSessionService workoutSessionService;
 
+  @Operation(
+      summary = "운동 일지 생성",
+      description = "트레이너가 운동 일지를 생성함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "404", description = "PT 계약이 존재하지 않음", content = @Content),
+      @ApiResponse(responseCode = "409", description = "해당 PT 수업에 대한 일지가 이미 존재함", content = @Content),
+      @ApiResponse(responseCode = "404", description = "운동 종류를 찾을 수 없음", content = @Content)
+  })
+  @PreAuthorize("hasRole('TRAINER')")
   @PostMapping
   public ResponseEntity<WorkoutSessionResponseDto> createWorkoutSession(
       @RequestBody WorkoutSessionCreateRequestDto dto
@@ -42,6 +60,17 @@ public class WorkoutSessionController {
     return ResponseEntity.ok(workoutSessionService.createWorkoutSession(dto));
   }
 
+  @Operation(
+      summary = "운동 일지 수정",
+      description = "트레이너가 운동 일지를 수정함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "404", description = "운동 일지를 찾을 수 없음", content = @Content),
+      @ApiResponse(responseCode = "404", description = "운동 종류를 찾을 수 없음", content = @Content),
+      @ApiResponse(responseCode = "404", description = "운동 기록을 찾을 수 없음", content = @Content)
+  })
+  @PreAuthorize("hasRole('TRAINER')")
   @PutMapping
   public ResponseEntity<WorkoutSessionResponseDto> updateWorkoutSession(
       @RequestBody WorkoutSessionUpdateRequestDto dto
@@ -49,6 +78,15 @@ public class WorkoutSessionController {
     return ResponseEntity.ok(workoutSessionService.updateWorkoutSession(dto));
   }
 
+  @Operation(
+      summary = "운동 종류 삭제",
+      description = "트레이너가 운동 일지를 수정함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "404", description = "운동 일지를 찾을 수 없음", content = @Content)
+  })
+  @PreAuthorize("hasRole('TRAINER')")
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteWorkoutSession(
       @PathVariable Long id
@@ -57,6 +95,14 @@ public class WorkoutSessionController {
     return ResponseEntity.ok().build();
   }
 
+  @Operation(
+      summary = "운동 일지 목록 조회",
+      description = "트레이너와 트레이니가 운동 일지의 목록을 조회함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공")
+  })
+  @PreAuthorize("hasRole('TRAINER') or hasRole('TRAINEE')")
   @GetMapping("/trainees/{id}")
   public ResponseEntity<Page<WorkoutSessionListResponseDto>> getWorkoutSessions(
       @PathVariable Long id,
@@ -67,6 +113,15 @@ public class WorkoutSessionController {
     return ResponseEntity.ok(workoutSessionService.getWorkoutSessions(id, pageable));
   }
 
+  @Operation(
+      summary = "운동 일지 상세 조회",
+      description = "트레이너와 트레이니가 운동 일지를 상세 조회함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "404", description = "운동 일지를 찾을 수 없음", content = @Content)
+  })
+  @PreAuthorize("hasRole('TRAINER') or hasRole('TRAINEE')")
   @GetMapping("/{id}")
   public ResponseEntity<WorkoutSessionResponseDto> getWorkoutSessionDetails(
       @PathVariable Long id
@@ -74,6 +129,17 @@ public class WorkoutSessionController {
     return ResponseEntity.ok(workoutSessionService.getWorkoutSessionDetails(id));
   }
 
+  @Operation(
+      summary = "운동 일지 이미지 업로드",
+      description = "트레이너가 운동 일지에 자세 사진을 업로드함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "404", description = "운동 일지를 찾을 수 없음", content = @Content),
+      @ApiResponse(responseCode = "413", description = "사진 업로드 개수는 10개까지 가능", content = @Content),
+      @ApiResponse(responseCode = "415", description = "사진 확장자 타입은 jpeg와 png만 가능", content = @Content)
+  })
+  @PreAuthorize("hasRole('TRAINER')")
   @PutMapping("/photos")
   public ResponseEntity<WorkoutImageResponseDto> uploadWorkoutImage(
       @RequestPart("sessionId") Long sessionId,
@@ -84,6 +150,17 @@ public class WorkoutSessionController {
     return ResponseEntity.ok(workoutSessionService.uploadWorkoutImage(dto));
   }
 
+  @Operation(
+      summary = "운동 일지 동영상 업로드",
+      description = "트레이너가 운동 일지에 동영상을 업로드함"
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "404", description = "운동 일지를 찾을 수 없음", content = @Content),
+      @ApiResponse(responseCode = "413", description = "동영상 업로드 개수는 10개까지 가능", content = @Content),
+      @ApiResponse(responseCode = "415", description = "동영상 확장자 타입은 mp4만 가능", content = @Content)
+  })
+  @PreAuthorize("hasRole('TRAINER')")
   @PutMapping("/videos")
   public ResponseEntity<WorkoutVideoResponseDto> uploadWorkoutVideo(
       @RequestPart("sessionId") Long sessionId,
