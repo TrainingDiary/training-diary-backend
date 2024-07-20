8aecc39bb6eb24fdfa3f022029bd949600c2119d
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-20 15:31:55 +0800

    feat: 식단 존재하지 않을떄 exception

---
+++ src/main/java/com/project/trainingdiary/exception/impl/DietNotExistException.java
@@ -0,0 +1,11 @@
+package com.project.trainingdiary.exception.impl;
+
+import com.project.trainingdiary.exception.GlobalException;
+import org.springframework.http.HttpStatus;
+
+public class DietNotExistException extends GlobalException {
+
+  public DietNotExistException() {
+    super(HttpStatus.NOT_FOUND, "해당 식단을 찾을 수 없습니다.");
+  }
+}
