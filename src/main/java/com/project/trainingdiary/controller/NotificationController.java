f223133ad6fdd4e90ce84ac0059629636453dcf9
Marcel Lee <chungchung1315@gmail.com>
2024-07-21 17:40:53 +0900

    feat: 알림 목록 조회의 정렬순서를 최신생성순으로 함

--- src/main/java/com/project/trainingdiary/controller/NotificationController.java
+++ src/main/java/com/project/trainingdiary/controller/NotificationController.java
@@ -13,6 +13,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -42,7 +44,7 @@ public class NotificationController {
       @RequestParam(defaultValue = "0") int page,
       @RequestParam(defaultValue = "20") int size
   ) {
-    Pageable pageable = PageRequest.of(page, size);
+    Pageable pageable = PageRequest.of(page, size, Sort.by(Direction.DESC, "createdAt"));
     return ResponseEntity.ok(notificationService.getNotificationList(pageable));
   }
 
