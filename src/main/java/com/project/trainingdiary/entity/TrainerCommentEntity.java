924575bc316219172094e53e690293902322ae92
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-22 22:40:31 +0800

    feat: 트레이너 댓글 엔티티 추가

---
+++ src/main/java/com/project/trainingdiary/entity/TrainerCommentEntity.java
@@ -0,0 +1,39 @@
+package com.project.trainingdiary.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "trainer_comment")
+public class TrainerCommentEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  private String comment;
+
+  @ManyToOne
+  @JoinColumn(name = "trainer_id")
+  private TrainerEntity trainer;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "diet_id")
+  private DietEntity diet;
+}
