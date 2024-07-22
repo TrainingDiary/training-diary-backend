9b0424628bf9c74b209d3f76447b58c1311e20bc
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-22 22:38:55 +0800

    feat: swagger용 schema 추가

--- src/main/java/com/project/trainingdiary/dto/request/diet/CreateDietRequestDto.java
+++ src/main/java/com/project/trainingdiary/dto/request/diet/CreateDietRequestDto.java
@@ -1,5 +1,7 @@
 package com.project.trainingdiary.dto.request.diet;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Builder
 public class CreateDietRequestDto {
 
+  @Schema(example = "6월 27일 오늘 점심 식단입니다!")
+  @NotNull(message = "content을 입력하세요")
   private String content;
+
+  @Schema(example = "image1.png")
+  @NotNull(message = "image를 업로드하세요.")
   private MultipartFile image;
 }
