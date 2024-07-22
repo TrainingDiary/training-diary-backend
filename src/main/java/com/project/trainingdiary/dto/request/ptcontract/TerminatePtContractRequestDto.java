c3f6dfc8b3b4780cb5a60d139a954b6da115dc17
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-22 21:23:31 +0800

    refactor: 해당 dto 일관성 있게 변경

---
+++ src/main/java/com/project/trainingdiary/dto/request/ptcontract/TerminatePtContractRequestDto.java
@@ -0,0 +1,15 @@
+package com.project.trainingdiary.dto.request.ptcontract;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TerminatePtContractRequestDto {
+
+  @NotNull(message = "PT 계약 id를 입력해주세요")
+  @Schema(example = "1")
+  private Long ptContractId;
+}
