c0643ebc2430c91590a40c8ec90e8c6dd34848f3
Hyunju Lee <guswnee00@gmail.com>
2024-07-23 22:10:26 +0900

    feat: 동영상 썸네일 필드 추가

--- src/main/java/com/project/trainingdiary/dto/response/workout/session/WorkoutSessionResponseDto.java
+++ src/main/java/com/project/trainingdiary/dto/response/workout/session/WorkoutSessionResponseDto.java
@@ -5,9 +5,12 @@ import static com.project.trainingdiary.model.type.WorkoutMediaType.VIDEO;
 
 import com.project.trainingdiary.entity.WorkoutMediaEntity;
 import com.project.trainingdiary.entity.WorkoutSessionEntity;
+import com.project.trainingdiary.util.ConvertCloudFrontUrlUtil;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +30,7 @@ public class WorkoutSessionResponseDto {
   private String specialNote;
   private List<WorkoutResponseDto> workouts;
   private List<String> photoUrls;
+  private List<String> thumbnailUrls;   // 동영상 썸네일
   private List<String> videoUrls;
 
   public static WorkoutSessionResponseDto fromEntity(WorkoutSessionEntity entity) {
@@ -34,21 +38,26 @@ public class WorkoutSessionResponseDto {
     List<WorkoutResponseDto> workoutDtos = entity.getWorkouts().stream()
         .map(WorkoutResponseDto::fromEntity).toList();
 
-    List<WorkoutMediaEntity> workoutMedia = entity.getWorkoutMedia();
-    List<String> photoUrls = new ArrayList<>();
-    List<String> videoUrls = new ArrayList<>();
+    List<WorkoutMediaEntity> workoutMedia = Optional.ofNullable(entity.getWorkoutMedia())
+        .orElse(Collections.emptyList());
 
-    if (workoutMedia != null && !workoutMedia.isEmpty()) {
-      photoUrls = workoutMedia.stream()
-          .filter(media -> media.getMediaType().equals(IMAGE))
-          .map(WorkoutMediaEntity::getOriginalUrl)
-          .toList();
+    List<String> photoUrls = workoutMedia.stream()
+        .filter(media -> media.getMediaType().equals(IMAGE))
+        .map(WorkoutMediaEntity::getOriginalUrl)
+        .map(ConvertCloudFrontUrlUtil::convertToCloudFrontUrl)
+        .toList();
 
-      videoUrls = workoutMedia.stream()
-          .filter(media -> media.getMediaType().equals(VIDEO))
-          .map(WorkoutMediaEntity::getOriginalUrl)
-          .toList();
-    }
+    List<String> thumbnailUrls = workoutMedia.stream()
+        .filter(media -> media.getMediaType().equals(VIDEO))
+        .map(WorkoutMediaEntity::getThumbnailUrl)
+        .map(ConvertCloudFrontUrlUtil::convertToCloudFrontUrl)
+        .toList();
+
+    List<String> videoUrls = workoutMedia.stream()
+        .filter(media -> media.getMediaType().equals(VIDEO))
+        .map(WorkoutMediaEntity::getOriginalUrl)
+        .map(ConvertCloudFrontUrlUtil::convertToCloudFrontUrl)
+        .toList();
 
     return WorkoutSessionResponseDto.builder()
         .sessionId(entity.getId())
@@ -57,6 +66,7 @@ public class WorkoutSessionResponseDto {
         .specialNote(entity.getSpecialNote())
         .workouts(workoutDtos)
         .photoUrls(photoUrls.isEmpty() ? new ArrayList<>() : photoUrls)
+        .thumbnailUrls(thumbnailUrls.isEmpty() ? new ArrayList<>() : thumbnailUrls)
         .videoUrls(videoUrls.isEmpty() ? new ArrayList<>() : videoUrls)
         .build();
 
