157345eb0f3c7139d7178d8ca72644e3c84f8578
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-26 00:29:39 +0800

    feat: userdetails 재사용 위해서 필드 추가

--- src/main/java/com/project/trainingdiary/model/UserPrincipal.java
+++ src/main/java/com/project/trainingdiary/model/UserPrincipal.java
@@ -2,33 +2,58 @@ package com.project.trainingdiary.model;
 
 import com.project.trainingdiary.entity.TraineeEntity;
 import com.project.trainingdiary.entity.TrainerEntity;
-import java.util.Collection;
-import java.util.Collections;
+import com.project.trainingdiary.model.type.UserRoleType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
 @AllArgsConstructor
 public class UserPrincipal implements UserDetails {
 
+  private final Long id;
   private final String email;
   private final String password;
+  private final String name;
+  private final UserRoleType role;
+  private final boolean unreadNotification;
   private final Collection<? extends GrantedAuthority> authorities;
+  private final boolean isTrainer;
+  private final TrainerEntity trainer;
+  private final TraineeEntity trainee;
 
   public static UserPrincipal create(TraineeEntity trainee) {
     return new UserPrincipal(
+        trainee.getId(),
         trainee.getEmail(),
         trainee.getPassword(),
-        Collections.singletonList(new SimpleGrantedAuthority("ROLE_TRAINEE"))
+        trainee.getName(),
+        UserRoleType.TRAINEE,
+        trainee.isUnreadNotification(),
+        Collections.singletonList(new SimpleGrantedAuthority("ROLE_TRAINEE")),
+        false,
+        null,
+        trainee
     );
   }
 
   public static UserPrincipal create(TrainerEntity trainer) {
     return new UserPrincipal(
+        trainer.getId(),
         trainer.getEmail(),
         trainer.getPassword(),
-        Collections.singletonList(new SimpleGrantedAuthority("ROLE_TRAINER"))
+        trainer.getName(),
+        UserRoleType.TRAINER,
+        trainer.isUnreadNotification(),
+        Collections.singletonList(new SimpleGrantedAuthority("ROLE_TRAINER")),
+        true,
+        trainer,
+        null
     );
   }
 
