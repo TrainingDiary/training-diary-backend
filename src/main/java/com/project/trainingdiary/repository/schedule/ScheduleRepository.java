ae9d787038eedd956629b5c613c887047632e490
Marcel Lee <chungchung1315@gmail.com>
2024-07-26 19:39:18 +0900

    fix: PT 계약 종료 시 미래에 남아있던 예약 일정을 모두 제거함

--- src/main/java/com/project/trainingdiary/repository/schedule/ScheduleRepository.java
+++ src/main/java/com/project/trainingdiary/repository/schedule/ScheduleRepository.java
@@ -44,4 +44,15 @@ public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long>,
       @Param("startAt1") LocalDateTime startAt1,
       @Param("startAt2") LocalDateTime startAt2
   );
+
+  @Query("select s "
+      + "from schedule s "
+      + "where s.trainer.id = :trainerId "
+      + "and s.ptContract.trainee.id = :traineeId "
+      + "and s.startAt >= :startAt")
+  List<ScheduleEntity> findByTraineeIdAndDateAfter(
+      @Param("trainerId") long trainerId,
+      @Param("traineeId") long traineeId,
+      @Param("startAt") LocalDateTime startAt
+  );
 }
