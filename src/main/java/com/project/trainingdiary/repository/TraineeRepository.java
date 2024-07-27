16ca680b1edb519c2ea56bb74962499d4cb1e1d8
Lee Kyoo Min <58134412+kyoo0115@users.noreply.github.com>
2024-07-27 15:30:58 +0800

    fix: 트레이니 계약 없이 호출 가능하도록 수정

--- src/main/java/com/project/trainingdiary/repository/TraineeRepository.java
+++ src/main/java/com/project/trainingdiary/repository/TraineeRepository.java
@@ -1,10 +1,15 @@
 package com.project.trainingdiary.repository;
 
 import com.project.trainingdiary.entity.TraineeEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TraineeRepository extends JpaRepository<TraineeEntity, Long> {
 
   Optional<TraineeEntity> findByEmail(String email);
+
+  @Query("SELECT t FROM trainee t LEFT JOIN FETCH t.inBodyRecords WHERE t.id = :id")
+  Optional<TraineeEntity> findByIdWithInBodyRecords(@Param("id") Long id);
 }
