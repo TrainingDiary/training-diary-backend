494a1c5e1811cfdbb0fb20097c4f8b3703ea1dea
Hyunju Lee <guswnee00@gmail.com>
2024-07-30 17:23:05 +0900

    fix: MultipartFile 자체를 인코딩하는 방식으로 변경

--- src/main/java/com/project/trainingdiary/util/VideoUtil.java
+++ src/main/java/com/project/trainingdiary/util/VideoUtil.java
@@ -1,8 +1,13 @@
 package com.project.trainingdiary.util;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import org.springframework.web.multipart.MultipartFile;
 
 public class VideoUtil {
 
@@ -11,14 +16,27 @@ public class VideoUtil {
   private static final String VIDEO_THUMBNAIL_WIDTH = "360:-1";
   private static final String VIDEO_THUMBNAIL_HEIGHT = "-1:360";
 
-  public static boolean isVerticalVideo(String inputUrl) throws IOException, InterruptedException {
+  public static boolean isVerticalVideo(MultipartFile file)
+      throws IOException, InterruptedException {
+    File tempFile = File.createTempFile("video", ".tmp");
+
+    try (InputStream inputStream = file.getInputStream()) {
+      try (OutputStream outputStream = new FileOutputStream(tempFile)) {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+          outputStream.write(buffer, 0, bytesRead);
+        }
+      }
+    }
+
     ProcessBuilder processBuilder = new ProcessBuilder(
         "ffprobe",
         "-v", "error",
         "-select_streams", "v:0",
         "-show_entries", "stream=width,height",
         "-of", "default=noprint_wrappers=1:nokey=1",
-        inputUrl
+        tempFile.getAbsolutePath()
     );
 
     Process process = processBuilder.start();
@@ -26,6 +44,8 @@ public class VideoUtil {
     String widthLine = reader.readLine();
     String heightLine = reader.readLine();
     process.waitFor();
+
+    tempFile.delete();
 
     if (widthLine != null && heightLine != null) {
       int width = Integer.parseInt(widthLine);
@@ -36,15 +56,27 @@ public class VideoUtil {
     return false;
   }
 
-  public static String encodeVideo(String inputUrl, String outputUrl)
+  public static String encodeVideo(MultipartFile file, String outputUrl)
       throws IOException, InterruptedException {
-    boolean isVertical = isVerticalVideo(inputUrl);
+    boolean isVertical = isVerticalVideo(file);
+
     ProcessBuilder processBuilder;
+    File tempFile = File.createTempFile("video", ".tmp");
+
+    try (InputStream inputStream = file.getInputStream()) {
+      try (OutputStream outputStream = new FileOutputStream(tempFile)) {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+          outputStream.write(buffer, 0, bytesRead);
+        }
+      }
+    }
 
     if (isVertical) {
       processBuilder = new ProcessBuilder(
           "ffmpeg",
-          "-i", inputUrl,
+          "-i", tempFile.getAbsolutePath(),
           "-vf", "scale=" + VIDEO_QUALITY_HEIGHT,
           "-preset", "medium",
           outputUrl
@@ -52,7 +84,7 @@ public class VideoUtil {
     } else {
       processBuilder = new ProcessBuilder(
           "ffmpeg",
-          "-i", inputUrl,
+          "-i", tempFile.getAbsolutePath(),
           "-vf", "scale=" + VIDEO_QUALITY_WIDTH,
           "-preset", "medium",
           outputUrl
@@ -62,18 +94,33 @@ public class VideoUtil {
     Process process = processBuilder.start();
     process.waitFor();
 
+    tempFile.delete();
+
     return outputUrl;
+
   }
 
-  public static String generateThumbnail(String inputUrl, String outputUrl)
+  public static String generateThumbnail(MultipartFile file, String outputUrl)
       throws IOException, InterruptedException {
-    boolean isVertical = isVerticalVideo(inputUrl);
+    boolean isVertical = isVerticalVideo(file);
+
     ProcessBuilder processBuilder;
+    File tempFile = File.createTempFile("video", ".tmp");
+
+    try (InputStream inputStream = file.getInputStream()) {
+      try (OutputStream outputStream = new FileOutputStream(tempFile)) {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+          outputStream.write(buffer, 0, bytesRead);
+        }
+      }
+    }
 
     if (isVertical) {
       processBuilder = new ProcessBuilder(
           "ffmpeg",
-          "-i", inputUrl,
+          "-i", tempFile.getAbsolutePath(),
           "-ss", "00:00:01.000",
           "-vframes", "1",
           "-vf", "scale=" + VIDEO_THUMBNAIL_WIDTH,
@@ -83,7 +130,7 @@ public class VideoUtil {
     } else {
       processBuilder = new ProcessBuilder(
           "ffmpeg",
-          "-i", inputUrl,
+          "-i", tempFile.getAbsolutePath(),
           "-ss", "00:00:01.000",
           "-vframes", "1",
           "-vf", "scale=" + VIDEO_THUMBNAIL_HEIGHT,
@@ -94,6 +141,8 @@ public class VideoUtil {
 
     Process process = processBuilder.start();
     process.waitFor();
+
+    tempFile.delete();
 
     return outputUrl;
   }
